d04cf9022a7751ca83da98db4dd4939cfe132f4c
rassi0429 <taitokokoa+rassi@gmail.com>
2023-08-29 19:24:01 +0900

    fix: idiot 3

--- main.mjs
+++ main.mjs
@@ -3,7 +3,7 @@ import { summaly } from 'summaly';
 import cors from 'cors';
 
 const app = express();
-app.listen(process.env.PORT, (srv) => console.log(`Server running on port ${srv.address().port}`));
+app.listen(process.env.PORT, () => console.log(`Server running`));
 
 // cors from environment variable
 const corsValiable = process.env.CORS || '*';
